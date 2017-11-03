2b16800706959a34291992b7a179050f4395f502
Kaio Max <kaiomaks@gmail.com>
2017-11-02 21:23:08 -0300

    Add base layout styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 
 import 'styles/app.css';
 
-import styles from './app.css';
-
 export default class App extends Component {
   render() {
     return (
-      <header className={ styles.myClass }>
-        <h1>App</h1>
+      <header>
+        <h1>MovieIt</h1>
       </header>
     );
   }
